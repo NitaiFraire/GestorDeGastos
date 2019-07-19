0a2ea4d2f4cc8889ab5bc74e3b5d88d19eb9271c
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-07-19 12:38:00 -0500

    mostrando error si algun campo esta vacio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // variables
 const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
 
@@ -38,11 +39,36 @@ class Interfaz{
 
     }
 
+    imprimirMensaje(mensaje, tipo){
+
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        divMensaje.appendChild(document.createTextNode(mensaje));
+
+        // insertar div en el dom
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        // eliminar alert despues de 3 segundos
+        setTimeout(function(){
+
+            document.querySelector('.primario .alert').remove();
+            formulario.reset();
+
+        }, 3000)
+    }
 }
 
 
 // event listeners
 
+// insertar presupuesto al cargar la pagina
 document.addEventListener('DOMContentLoaded', function(){
 
     if(presupuestoUsuario === null || presupuestoUsuario === ''){
@@ -56,6 +82,32 @@ document.addEventListener('DOMContentLoaded', function(){
         // instancias interfaz
         const gui = new Interfaz();
         gui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
+    }
+
+});
+
+
+// obtener valores del formulario
+formulario.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    // leer del formulario de gastos
+    const nombreGasto = document.querySelector('#gasto').value;
+    const cantidadGasto = document.querySelector('#cantidad').value;
+
+    // instanciar la interfaz
+    const gui = new Interfaz();
+
+    // validar datos del formulario
+    if(nombreGasto === '' || cantidadGasto === ''){
+
+        console.log('error');
+        // 2 parametros: mensaje y tipo 
+        gui.imprimirMensaje('Hubo un error', 'error');
+    }else{
+        
+        console.log('no error');
     }
 
 });
